a6bc7504e061312fcb2006d5ce83019567fb48ab
Collin Osborne <crosborn3@protonmail.com>
2022-02-25 16:54:17 -0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="sticky">
+      <nav className="navbar">
+        <div className="wrapper">
+          <div className="nav-header">
+              <div className="nav-logo">
+                IKUZA
+              </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
